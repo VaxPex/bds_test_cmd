ee73d196325489858c0be318c042cc769b834f10
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-06-19 19:35:13 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # bds_test_cmd
 adding /test command in bds using bedrock-protocol
 
-mc v1.19.0
+install https://www.minecraft.net/en-us/download/server/bedrock the windows ver and extract it and rename it to bedrock_server
---
+++ index.js
@@ -0,0 +1,49 @@
+// process.env.DEBUG = 'minecraft-protocol';
+
+const { Relay } = require('bedrock-protocol');
+const child_process = require('child_process');
+const fs = require("fs");
+
+const bedrock_server_path = __dirname + "/bedrock_server/";
+
+fs.writeFileSync(bedrock_server_path + "server.properties", fs.readFileSync(__dirname + "/modefied_files/server.properties").toString('utf-8'));
+
+child_process.exec(bedrock_server_path + "bedrock_server.exe", (error, stderr, stdout) => {
+	if (error) {
+		throw new Error(error.message);
+	}
+	if (stderr) {
+		console.log("stderr: " + stderr);
+	}
+});
+
+console.log("Started...");
+
+const relay = new Relay({
+	host: "0.0.0.0",
+	port: 19132,
+	destination: {
+		host: "127.0.0.1",
+		port: 65534,
+		offline: false
+	}
+})
+relay.canLong = console.debug;
+relay.listen();
+
+relay.on("connect", player => {
+	player.on('clientbound', ({ name, params }) => {
+		// console.log("clientbound: " + name);
+		if (name === "available_commands") {
+params.command_data.push({name: 'test', description: 'test command', flags: 192, permission_level: 0, alias: -1, overloads: []});
+			//console.log(params);
+		}
+	});
+	player.on('serverbound', ({ name, params }) => {
+		if (name === "text") {
+			// console.log(params);
+		} else {
+			// console.log(name);
+		}
+	});
+});
