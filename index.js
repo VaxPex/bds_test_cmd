3e1e2125be8def9721dc44da489e9161723c9f10
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-06-21 13:08:20 +0300

    command overloads

--- index.js
+++ index.js
@@ -31,11 +31,31 @@ const relay = new Relay({
 relay.canLong = console.debug;
 relay.listen();
 
+function generateCommandOverloads(parameterName = 'args', valueType = 53, enumType = 'valid', optinal = true, options = {
+	unused: 0,
+	collapse_enum: 0,
+	has_semantic_constraint: 0,
+	as_chained_command: 0,
+	unknown2: 0
+}) {
+	return [
+				[
+					{
+						parameter_name: parameterName,
+						value_type: valueType,
+						enum_type: enumType,
+						optinal: optinal,
+						options: options
+					}
+				]
+		];
+}
+
 relay.on("connect", player => {
 	player.on('clientbound', ({ name, params }) => {
 		// console.log("clientbound: " + name);
 		if (name === "available_commands") {
-params.command_data.push({name: 'test', description: 'test command', flags: 192, permission_level: 0, alias: -1, overloads: []});
+			params.command_data.push({name: 'test', description: 'test command', flags: 192, permission_level: 0, alias: -1, overloads: generateCommandOverloads()});
 			//console.log(params);
 		}
 	});
